676341720953cb7839c5dd64e36cc8977b755074
seradiazpin <seradiazpin@unal.edu.co>
2020-03-06 22:38:01 -0500

    generate multiple images}

--- pretrained_example.py
+++ pretrained_example.py
@@ -14,13 +14,13 @@ import PIL.Image
 import dnnlib
 import dnnlib.tflib as tflib
 import config
-
+import random
 def main():
     # Initialize TensorFlow.
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = os.path.abspath("results/00004-sgan-poke-1gpu/network-snapshot-004004.pkl") 
+    url = os.path.abspath("results/00004-sgan-poke-1gpu/network-snapshot-015204.pkl") 
     #'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ' # karras2019stylegan-ffhq-1024x1024.pkl
     #with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
     with open(url, 'rb') as f:
@@ -33,17 +33,20 @@ def main():
     Gs.print_layers()
 
     # Pick latent vector.
-    rnd = np.random.RandomState(5)
-    latents = rnd.randn(1, Gs.input_shape[1])
+
 
     # Generate image.
-    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+    
+    for i in range(120):
+        rnd = np.random.RandomState(random.randint(1,1000))
+        latents = rnd.randn(1, Gs.input_shape[1])
+        fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+        images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
-    # Save image.
-    os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+        # Save image.
+        os.makedirs(config.result_dir, exist_ok=True)
+        png_filename = os.path.join(config.result_dir+"/test", 'example'+str(i)+'.png')
+        PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
     main()
